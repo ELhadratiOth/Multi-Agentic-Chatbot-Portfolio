a575ad7d2e3992e06c1006394c25fa3770f1db50
Othman El hadrati <othmandone@gmail.com>
2025-02-25 18:57:26 +0000

    headless credentials

--- utils/tools.py
+++ utils/tools.py
@@ -6,6 +6,9 @@ import base64
 from datetime import datetime
 from langchain_community.agent_toolkits.gmail.toolkit import GmailToolkit
 from langchain_google_community.gmail.utils import build_resource_service, get_gmail_credentials
+import base64
+import json
+from google.oauth2.service_account import Credentials
 from langchain.agents import initialize_agent, AgentType
 from langchain.prompts import  SystemMessagePromptTemplate
 from utils.model import  llm_lang
@@ -98,11 +101,21 @@ def get_github_file(repo:str) -> str:
     
     
 #langchain toolkit
-credentials = get_gmail_credentials(
-    token_file="/tmp/token.json",
-    scopes=["https://mail.google.com/"],
-    client_secrets_file="/etc/secrets/credentials.json",
-)
+from google.oauth2.service_account import Credentials
+
+
+def get_service_account_credentials():
+    encoded_key = os.getenv("SERVICE_ACCOUNT_KEY")
+    decoded_key = base64.b64decode(encoded_key).decode("utf-8")
+    credentials_info = json.loads(decoded_key)
+    credentials = Credentials.from_service_account_info(
+        credentials_info,
+        scopes=["https://mail.google.com/"]
+    )
+    return credentials
+
+credentials = get_service_account_credentials()
+
 # credentials = get_gmail_credentials(
 #     token_file="./logs/token.json",
 #     scopes=["https://mail.google.com/"],
